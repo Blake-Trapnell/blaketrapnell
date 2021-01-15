f871f3939f785de9540dd6280d5d41f4e316f80d
Blake Trapnell <blake.trapnell103@gmail.com>
2021-01-14 23:57:04 -0700

    added blake to moon animation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,19 +7,42 @@ const About = () => {
 
     const blakeToCircle = () => {
         const tl = anime.timeline({
-            targets:[".blake-text"]
+            targets:[".blake-text", ".about--moon"]
         })
 		tl.add({
             fontSize: 0,
-			easing: "linear",
-			delay: (el, i) => 50 * i,
+            duration: 2000,
+            opacity: 1,
         })
+        .add({
+            targets: ".about--moon",
+            height: 100,
+            width: 100,
+            duration: 1000,
+            top: 20,
+            right: 20,
+            opacity: 1
+        })
+        .add({
+            targets: ".about--moon-crescent",
+            height: 70,
+            width: 70,
+            top: 0,
+            left: 35,
+            opacity: 1,
+            easing: "linear"
+        })
+
+
     };
     
     return (
         <div className={"about--outer-container"}>
             <header>
                 <h2 onClick={()=>blakeToCircle()} className={"blake-text"}>Blake Trapnell</h2>
+                <div className={"about--moon"}>
+                    <div className={"about--moon-crescent"}></div>
+                </div>
             </header>
             <article>
                 Hi i'm Blake, a web developer. I enjoy making fun, beautiful, interactive websites.
